dd4b826a6b1c802536f2ded190b3feed3e63b7ae
Richard Michael Coo <rmcoo@finattune.com.au>
2026-02-04 20:57:14 +0800

    refactor: refactor pure function utils in favor of getters

---
+++ src/common/models/page-info.model.test.ts
@@ -0,0 +1,108 @@
+import { PageInfo } from './page-info.model';
+
+describe('createPaginationMetadata', () => {
+  it('returns correct metadata for the first page', () => {
+    // Arrange
+    const totalItemsCount = 100;
+    const currentPage = 1;
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(true);
+    expect(result.hasPreviousPage).toBe(false);
+    expect(result.totalPageCount).toBe(10);
+  });
+
+  it('returns correct metadata for the last page', () => {
+    // Arrange
+    const totalItemsCount = 100;
+    const currentPage = 10;
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(false);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.totalPageCount).toBe(10);
+  });
+
+  it('returns correct metadata for the last page, when itemsPerPage does not divide totalItemsCount evenly', () => {
+    // Arrange
+    const totalItemsCount = 95;
+    const currentPage = 10; // Last page
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(false);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.totalPageCount).toBe(10);
+  });
+
+  it('returns correct metadata for a middle page', () => {
+    // Arrange
+    const totalItemsCount = 100;
+    const currentPage = 5;
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(true);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.totalPageCount).toBe(10);
+  });
+
+  it('returns correct metadata when there are no items', () => {
+    // Arrange
+    const totalItemsCount = 0;
+    const currentPage = 1;
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(false);
+    expect(result.hasPreviousPage).toBe(false);
+    expect(result.totalPageCount).toBe(0);
+  });
+
+  it('returns correct metadata when itemsPerPage is greater than totalItemsCount', () => {
+    // Arrange
+    const totalItemsCount = 5;
+    const currentPage = 1;
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(false);
+    expect(result.hasPreviousPage).toBe(false);
+    expect(result.totalPageCount).toBe(1);
+  });
+
+  it('returns correct metadata when currentPage is greater than totalPageCount', () => {
+    // Arrange
+    const totalItemsCount = 50;
+    const currentPage = 6; // More than total pages
+    const itemsPerPage = 10;
+
+    // Act
+    const result = new PageInfo({ totalItemsCount, currentPage, itemsPerPage });
+
+    // Assert
+    expect(result.hasNextPage).toBe(false);
+    expect(result.hasPreviousPage).toBe(true);
+    expect(result.totalPageCount).toBe(5);
+  });
+});
--- src/common/models/page-info.model.ts
+++ src/common/models/page-info.model.ts
@@ -1,4 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Expose } from 'class-transformer';
 
 /**
  * Information about the current page. While the naming is the same, this is not comformant to the
@@ -9,24 +10,49 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class PageInfo {
   /**
+   * Items per page.
+   */
+  @Field(() => Int)
+  readonly itemsPerPage: number;
+
+  /**
    * The total number of items available.
    */
   @Field(() => Int)
-  totalItemsCount: number;
+  readonly totalItemsCount: number;
+
+  /**
+   * The current page number (1-based).
+   */
+  @Field(() => Int)
+  readonly currentPage: number;
 
   /**
    * Whether there's a next page.
    */
-  hasNextPage: boolean;
+  @Expose()
+  get hasNextPage(): boolean {
+    return this.currentPage < this.totalPageCount;
+  }
 
   /**
-   * Whether there's a previus page.
+   * Whether there's a previous page.
    */
-  hasPreviousPage: boolean;
+  @Expose()
+  get hasPreviousPage(): boolean {
+    return this.currentPage > 1;
+  }
 
   /**
    * The total number of pages available.
    */
   @Field(() => Int)
-  totalPageCount: number;
+  @Expose()
+  get totalPageCount(): number {
+    return Math.ceil(this.totalItemsCount / this.itemsPerPage);
+  }
+
+  constructor(data: Pick<PageInfo, 'itemsPerPage' | 'totalItemsCount' | 'currentPage'>) {
+    Object.assign(this, data);
+  }
 }
